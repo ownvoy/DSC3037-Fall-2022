1110e18e12d23df0379d1dfc9fbb97ec6d82b9b8
wonjun <ownvoy@g.skku.edu>
2024-11-01 13:29:18 +0900

    회원가입 page restore

--- models.py
+++ models.py
@@ -13,29 +13,29 @@ class AuthGroup(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'auth_group'
+        db_table = "auth_group"
 
 
 class AuthGroupPermissions(models.Model):
     id = models.BigAutoField(primary_key=True)
     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+    permission = models.ForeignKey("AuthPermission", models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
+        db_table = "auth_group_permissions"
+        unique_together = (("group", "permission"),)
 
 
 class AuthPermission(models.Model):
     name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    content_type = models.ForeignKey("DjangoContentType", models.DO_NOTHING)
     codename = models.CharField(max_length=100)
 
     class Meta:
         managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
+        db_table = "auth_permission"
+        unique_together = (("content_type", "codename"),)
 
 
 class AuthUser(models.Model):
@@ -52,7 +52,7 @@ class AuthUser(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'auth_user'
+        db_table = "auth_user"
 
 
 class AuthUserGroups(models.Model):
@@ -62,8 +62,8 @@ class AuthUserGroups(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
+        db_table = "auth_user_groups"
+        unique_together = (("user", "group"),)
 
 
 class AuthUserUserPermissions(models.Model):
@@ -73,8 +73,8 @@ class AuthUserUserPermissions(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
+        db_table = "auth_user_user_permissions"
+        unique_together = (("user", "permission"),)
 
 
 class DjangoAdminLog(models.Model):
@@ -83,12 +83,14 @@ class DjangoAdminLog(models.Model):
     object_repr = models.CharField(max_length=200)
     action_flag = models.PositiveSmallIntegerField()
     change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    content_type = models.ForeignKey(
+        "DjangoContentType", models.DO_NOTHING, blank=True, null=True
+    )
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'django_admin_log'
+        db_table = "django_admin_log"
 
 
 class DjangoContentType(models.Model):
@@ -97,8 +99,8 @@ class DjangoContentType(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
+        db_table = "django_content_type"
+        unique_together = (("app_label", "model"),)
 
 
 class DjangoMigrations(models.Model):
@@ -109,7 +111,7 @@ class DjangoMigrations(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'django_migrations'
+        db_table = "django_migrations"
 
 
 class DjangoSession(models.Model):
@@ -119,51 +121,54 @@ class DjangoSession(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'django_session'
+        db_table = "django_session"
 
 
-class LiberalArtsSubject(models.Model):
-    course_code = models.TextField(blank=True, null=True, primary_key=True)
-    type_of_field = models.TextField(blank=True, null=True)
-    course_title = models.TextField(blank=True, null=True)
-    course_title_english = models.TextField(blank=True, null=True)
-    instructor = models.TextField(blank=True, null=True)
-    campus = models.TextField(blank=True, null=True)
-    degree_courses = models.TextField(blank=True, null=True)
-    classtime = models.TextField(blank=True, null=True)
-    type_of_class1 = models.TextField(blank=True, null=True)
-    credits_hrs_field = models.TextField(db_column='credits(Hrs)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
-    type_of_class2 = models.TextField(blank=True, null=True)
-    remarks = models.TextField(blank=True, null=True)
-    type_of_class3 = models.TextField(blank=True, null=True)
-    semester = models.TextField(blank=True, null=True, primary_key=True)
-
-    class Meta:
-        managed = True
-        db_table = 'liberal arts_subject'
-
-
-class MajorSubject(models.Model):
-    semester = models.TextField(blank=True, null=True)
-    major = models.CharField(max_length=255, blank=True, null=True)
-    college = models.CharField(max_length=255, blank=True, null=True)
-    degree_courses = models.TextField(blank=True, null=True)
-    course_code = models.TextField(blank=True, null=True)
-    course_title = models.TextField(blank=True, null=True)
-    course_title_english = models.TextField(blank=True, null=True)
-    instructor = models.TextField(blank=True, null=True)
-    campus = models.TextField(blank=True, null=True)
-    type_of_field2 = models.TextField(blank=True, null=True)
-    type_of_field3 = models.CharField(max_length=255, blank=True, null=True)
-    credits_hrs_field = models.TextField(db_column='credits(Hrs)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
-    classtime = models.TextField(blank=True, null=True)
-    type_of_class1 = models.TextField(blank=True, null=True)
-    type_of_class2 = models.TextField(blank=True, null=True)
-    remarks = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'major_subject'
+#
+#
+# class LiberalArtsSubject(models.Model):
+#     course_code = models.TextField(blank=True, null=True, primary_key=True)
+#     type_of_field = models.TextField(blank=True, null=True)
+#     course_title = models.TextField(blank=True, null=True)
+#     course_title_english = models.TextField(blank=True, null=True)
+#     instructor = models.TextField(blank=True, null=True)
+#     campus = models.TextField(blank=True, null=True)
+#     degree_courses = models.TextField(blank=True, null=True)
+#     classtime = models.TextField(blank=True, null=True)
+#     type_of_class1 = models.TextField(blank=True, null=True)
+#     credits_hrs_field = models.TextField(db_column='credits(Hrs)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
+#     type_of_class2 = models.TextField(blank=True, null=True)
+#     remarks = models.TextField(blank=True, null=True)
+#     type_of_class3 = models.TextField(blank=True, null=True)
+#     semester = models.TextField(blank=True, null=True, primary_key=True)
+#
+#     class Meta:
+#         managed = True
+#         db_table = 'liberal arts_subject'
+#
+#
+# class MajorSubject(models.Model):
+#     semester = models.TextField(blank=True, null=True)
+#     major = models.CharField(max_length=255, blank=True, null=True)
+#     college = models.CharField(max_length=255, blank=True, null=True)
+#     degree_courses = models.TextField(blank=True, null=True)
+#     course_code = models.TextField(blank=True, null=True)
+#     course_title = models.TextField(blank=True, null=True)
+#     course_title_english = models.TextField(blank=True, null=True)
+#     instructor = models.TextField(blank=True, null=True)
+#     campus = models.TextField(blank=True, null=True)
+#     type_of_field2 = models.TextField(blank=True, null=True)
+#     type_of_field3 = models.CharField(max_length=255, blank=True, null=True)
+#     credits_hrs_field = models.TextField(db_column='credits(Hrs)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
+#     classtime = models.TextField(blank=True, null=True)
+#     type_of_class1 = models.TextField(blank=True, null=True)
+#     type_of_class2 = models.TextField(blank=True, null=True)
+#     remarks = models.TextField(blank=True, null=True)
+#
+#     class Meta:
+#         managed = True
+#         db_table = 'major_subject'
+#
 
 
 class SkkuSubject(models.Model):
@@ -187,7 +192,7 @@ class SkkuSubject(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'skku_subject'
+        db_table = "skku_subject"
 
 
 class StudentAcademic(models.Model):
@@ -199,7 +204,7 @@ class StudentAcademic(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'student_academic'
+        db_table = "student_academic"
 
 
 class StudentMandatory(models.Model):
@@ -210,25 +215,29 @@ class StudentMandatory(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'student_mandatory'
+        db_table = "student_mandatory"
 
 
 class StudentPreference(models.Model):
     student = models.OneToOneField(StudentAcademic, models.DO_NOTHING, primary_key=True)
     instructor = models.CharField(max_length=50)
     morningclass = models.IntegerField()
-    courseratio = models.FloatField(db_column='courseRatio')  # Field name made lowercase.
+    courseratio = models.FloatField(
+        db_column="courseRatio"
+    )  # Field name made lowercase.
 
     class Meta:
         managed = False
-        db_table = 'student_preference'
+        db_table = "student_preference"
 
 
 class UserUser(models.Model):
-    student_id = models.OneToOneField(StudentAcademic, models.DO_NOTHING, primary_key=True)
+    student_id = models.OneToOneField(
+        StudentAcademic, models.DO_NOTHING, primary_key=True
+    )
     login_id = models.CharField(max_length=20)
     login_password = models.CharField(max_length=20)
 
     class Meta:
         managed = False
-        db_table = 'user_user'
+        db_table = "user_user"
